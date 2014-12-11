e02d2257e03203d200d3a7149ec9b9d25ded7c3a
btssio <btssio@btssio-VirtualBox>
2014-12-11 17:13:50 +0100

    Léo 2l3e 231b3o21zd664 51261	2	131	3	1	62	616

--- src/modele/dao/DAO_utilisateurs.java
+++ src/modele/dao/DAO_utilisateurs.java
@@ -18,23 +18,6 @@ import java.util.logging.Logger;
  * @author btssio
  */
 public class DAO_utilisateurs extends Connexion {
-    public Connexion(Connexion con) {
-        super(con, VISITEUR);
-    }
-
-    @Override
-    public int count() throws SQLException {
-        String query = "SELECT COUNT(*) AS count FROM "+this.tableName;
-        PreparedStatement counter;
-        try
-        {
-        counter = this.con.PrepareStatement(query);
-        ResultSet res = counter.executeQuery();
-        res.next();
-        return res.getInt("count");
-        }
-        catch(SQLException e){ throw e; }
-    }
     
     public DAO_utilisateurs(){
         try {
@@ -46,49 +29,12 @@ public class DAO_utilisateurs extends Connexion {
     
     public void listeUtilisateur() throws SQLException{
         
-             System.out.println("-------- Oracle JDBC Connection Testing ------");
- 
-		try {
- 
-			Class.forName("oracle.jdbc.driver.OracleDriver");
- 
-		} catch (ClassNotFoundException e) {
- 
-			System.out.println("Where is your Oracle JDBC Driver?");
-			e.printStackTrace();
-			return;
- 
-		}
- 
-		System.out.println("Oracle JDBC Driver Registered!");
- 
-		Connection connection = null;
- 
-		try {
- 
-			connection = DriverManager.getConnection(
-					"jdbc:oracle:thin:@localhost:1521:XE", "GSB",
-					"GSB");
- 
-		} catch (SQLException e) {
- 
-			System.out.println("Connection Failed! Check output console");
-			e.printStackTrace();
-			return;
- 
-		}
- 
-		if (connection != null) {
-			System.out.println("You made it, take control your database now!");
-		} else {
-			System.out.println("Failed to make connection!");
-		}
-	
-            Statement vSt = connection.createStatement();
+            Statement vSt = super.Connexion().createStatement();
             // Requête de sélection
             ResultSet vRs = vSt.executeQuery("SELECT v.VIS_PRENOM FROM VISITEUR v");
             while(vRs.next()){
             String vPrenom = vRs.getString(1);
+            
             System.out.println("Prenom="+vPrenom);
         }
     }

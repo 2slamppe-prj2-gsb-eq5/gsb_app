1ce81a9803a9ded30577d36e587a4ccff888b40d
btssio <btssio@btssio-VirtualBox>
2014-12-11 15:49:46 +0100

    12.49 ll

---
+++ src/test/modele/metier/Utilisateur.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package test.modele.metier;
+
+/**
+ *
+ * @author btssio
+ */
+public class Utilisateur {
+    private String matricule;
+    private String nom;
+    private String prenom;
+    private String adresse;
+    private String CP;
+    private String ville;
+    private String dateEmbauche;
+    private String code;
+    
+    
+    public Utilisateur(){
+        
+    }
+    
+    
+}

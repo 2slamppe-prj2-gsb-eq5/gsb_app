5e5742e58b03498b1a47285e2bf1d01b2aaad7c1
btssio <btssio@btssio-VirtualBox>
2014-12-11 16:29:12 +0100

    classe utilisateur

--- src/test/modele/metier/Utilisateur.java
+++ src/test/modele/metier/Utilisateur.java
@@ -18,11 +18,84 @@ public class Utilisateur {
     private String CP;
     private String ville;
     private String dateEmbauche;
-    private String code;
+    private String secCode;
+    private String labCode;
     
     
     public Utilisateur(){
         
+    }
+
+    public String getMatricule() {
+        return matricule;
+    }
+
+    public void setMatricule(String matricule) {
+        this.matricule = matricule;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getCP() {
+        return CP;
+    }
+
+    public void setCP(String CP) {
+        this.CP = CP;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+    public String getDateEmbauche() {
+        return dateEmbauche;
+    }
+
+    public void setDateEmbauche(String dateEmbauche) {
+        this.dateEmbauche = dateEmbauche;
+    }
+
+    public String getSecCode() {
+        return secCode;
+    }
+
+    public void setSecCode(String secCode) {
+        this.secCode = secCode;
+    }
+
+    public String getLabCode() {
+        return labCode;
+    }
+
+    public void setLabCode(String labCode) {
+        this.labCode = labCode;
     }
     
     
